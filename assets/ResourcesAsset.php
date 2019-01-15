13f8a4c566c1ef2f882a39eee683e2952dc4e601
Marc Stampfli <marc@zephir.ch>
2019-01-15 11:24:29 +0100

    Added zephir frontend resources; Added basic scss and js structure; Updated unglue and asset file

--- assets/ResourcesAsset.php
+++ assets/ResourcesAsset.php
@@ -10,25 +10,20 @@ class ResourcesAsset extends \luya\web\Asset
     public $sourcePath = '@app/resources';
     
     public $css = [
-        '//fonts.googleapis.com/css?family=Roboto:100,300,400',
-        '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css',
-        '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
-        'css/style.css'
+        'dist/main.css'
     ];
 
     public $js = [
-        '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'
+        'dist/main.js'
     ];
     
-    public $publishOptions = [
-        'only' => [
-            'css/*',
-            'js/*',
-        ]
-    ];
-
-
     public $depends = [
         'yii\web\JqueryAsset',
+    ];
+
+    public $publishOptions = [
+        'except' => [
+            '*.unglue'
+        ]
     ];
 }
